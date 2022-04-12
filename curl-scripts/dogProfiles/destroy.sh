c3211b5ac55ef99e20e711d23f80a3ed9f26a2a7
SakoPak <sako.taya@gmail.com>
2022-04-12 13:22:36 -0700

    Created and tested delete curl scripts

--- curl-scripts/dogProfiles/destroy.sh
+++ curl-scripts/dogProfiles/destroy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# sh curl-scripts/dogProfiles/destroy.sh
+
+TOKEN="1fc71b8ed525c6a0dddf7cadeef9eea4"
+ID="6255d6a4cd761170534ce41c"
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/dogProfiles"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
