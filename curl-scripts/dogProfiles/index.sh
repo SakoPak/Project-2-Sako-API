79b19b8406e985f8435cec3c7fcb1643cdc00d96
SakoPak <sako.taya@gmail.com>
2022-04-12 13:21:15 -0700

    Create and tested index curl scripts

--- curl-scripts/dogProfiles/index.sh
+++ curl-scripts/dogProfiles/index.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+# sh curl-scripts/dogProfiles/index.sh
+
+TOKEN="1fc71b8ed525c6a0dddf7cadeef9eea4"
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/dogProfiles"
 
 curl "${API}${URL_PATH}" \
   --include \
