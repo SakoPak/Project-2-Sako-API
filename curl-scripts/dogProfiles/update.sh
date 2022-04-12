ddce857f2e2dc4eba3f42e631f6f05c3f63a9b07
SakoPak <sako.taya@gmail.com>
2022-04-12 13:21:59 -0700

    Created and tested update curl scripts

--- curl-scripts/dogProfiles/update.sh
+++ curl-scripts/dogProfiles/update.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+# sh curl-scripts/dogProfiles/update.sh
+
+NAME="Bruno"
+AGE="6 years old"
+GENDER="Male"
+TYPE="Boxer"
+NOTES="He likes bacon."
+TOKEN="1fc71b8ed525c6a0dddf7cadeef9eea4"
+ID="6255d6a4cd761170534ce41c"
+
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/dogProfiles"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,8 +19,12 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
 --header "Authorization: Bearer ${TOKEN}" \
 --data '{
-    "example": {
-      "text": "'"${TEXT}"'"
+    "dog": {
+      "name": "'"${NAME}"'",
+      "age": "'"${AGE}"'",
+      "gender": "'"${GENDER}"'",
+      "type": "'"${TYPE}"'",
+      "notes": "'"${NOTES}"'"
     }
   }'
 
