af87c745dbaa32a913ed329327034851d4b86827
SakoPak <sako.taya@gmail.com>
2022-04-13 21:48:27 -0700

    Add show curl scripts

--- curl-scripts/dogProfiles/show.sh
+++ curl-scripts/dogProfiles/show.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+# sh curl-scripts/dogProfiles/show.sh
+
+TOKEN="1fc71b8ed525c6a0dddf7cadeef9eea4"
+
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/dogProfiles"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
