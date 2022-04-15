d0d021f4a264049edfa8b27ebe3d00b782385e3f
SakoPak <sako.taya@gmail.com>
2022-04-14 21:29:19 -0700

    Edits to dog profile routes

--- app/routes/dogProfiles_routes.js
+++ app/routes/dogProfiles_routes.js
@@ -19,7 +19,7 @@ const requireOwnership = customErrors.requireOwnership
 // this is middleware that will remove blank fields from `req.body`, e.g.
 // { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
 const removeBlanks = require('../../lib/remove_blank_fields')
-const dog = require('../models/dog')
+
 // passing this as a second argument to `router.<verb>` will make it
 // so that a token MUST be passed for that route to be available
 // it will also set `req.user`
@@ -31,15 +31,13 @@ const router = express.Router()
 // INDEX
 // GET /dogProfiles
 router.get('/dogProfiles', requireToken, (req, res, next) => {
-  Dog.find()
-    .then(dog => {
-      // `examples` will be an array of Mongoose documents
-      // we want to convert each one to a POJO, so we use `.map` to
-      // apply `.toObject` to each one
-      return dog.map(dog => dog.toObject())
-    })
+  Dog.find({owner: req.user.id})
+    // .then(dog => {
+    //   return dog.filter(profile => { return profile.owner === req.user.id })
+    // })
+    .then(dog => dog.map(dog => dog.toObject()))
     // respond with status 200 and JSON of the examples
-    .then(dog => res.status(200).json({ dog: dog}))
+    .then(dog => res.json({dog: dog}))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
