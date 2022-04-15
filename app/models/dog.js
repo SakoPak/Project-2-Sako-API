a0aa3c766fff34c3c12f4a5757615749b27e0e08
SakoPak <sako.taya@gmail.com>
2022-04-14 21:29:59 -0700

    Updated dog model

--- app/models/dog.js
+++ app/models/dog.js
@@ -1,36 +1,36 @@
 const mongoose = require('mongoose')
 
 const dogSchema = new mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-		},
-		age: {
-			type: String,
-			required: true,
-		},
-		gender: {
-			type: String,
-			required: true,
-		},
-		type: {
-			type: String,
-			required: true,
-		},
-		notes: {
-			type: String,
-			required: true,
-		},
-		owner: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'User',
-			required: true,
-		},
-	},
-	{
-		timestamps: true,
-	}
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    age: {
+      type: String,
+      required: true
+    },
+    gender: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    notes: {
+      type: String,
+      required: true
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
 )
 
 module.exports = mongoose.model('Dog', dogSchema)
