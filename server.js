d84efcc0aae16e818a1091a34cc03d19ea6e65e7
SakoPak <sako.taya@gmail.com>
2022-04-14 21:30:37 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const dogProfiles_routes = require('./app/routes/dogProfiles_routes')
+const dogProfiles = require('./app/routes/dogProfiles_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require middleware
@@ -56,7 +56,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(dogProfiles_routes)
+app.use(dogProfiles)
 app.use(userRoutes)
 
 // register error handling middleware
